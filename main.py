61f11dfefdaedff39ae25b7f2fb5d8105d8acd8a
tinali0 <xzl5423@psu.edu>
2020-08-25 12:07:09 +0000

    add more codes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 print('Hello World!')
 name = input("Enter your name: ")
+
 print("Hello, " + name + "!")
 age = input("Enter your age: ")
 age = int(age)
+print(age + 1)
+
 print("you will be " + str(age+1) + " next year")
+
+print(f"I will be {age+1} next year")
