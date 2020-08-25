8c29ec3fd03fdda74fd5162d845891550d2eef7c
tinali0 <xzl5423@psu.edu>
2020-08-25 11:59:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print('Hello World!')
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+age = input("Enter your age: ")
+age = int(age)
+print("you will be " + str(age+1) + " next year")
